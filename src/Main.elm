6d1da10f98dfd28562dbce2f5f56fe75bbd375e5
R4chel <rae84@cornell.edu>
2023-04-12 17:19:42 -0400

    getting going, still untested

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,11 @@
 module Main exposing (Atom(..), Model, main)
 
 import Browser
+import Browser.Events exposing (onKeyPress)
 import Color exposing (Color)
+import Dict exposing (Dict)
 import Html exposing (Html, button, div, text)
+import Json.Decode as Decode
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 
@@ -43,6 +46,108 @@ type Atom
     | B
 
 
+type alias Exp =
+    List Atom
+
+
+type alias Rules =
+    Atom -> Exp
+
+
+type alias LSystem =
+    { axiom : Exp
+    , rules : Rules
+    }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { lsystem : LSystem
+    , state : Exp
+    }
+
+
+
+-- INIT
+
+
+init : () -> ( Model, Cmd Msg )
+init () =
+    let
+        lsystem =
+            { axiom = [ A ]
+            , rules =
+                \atom ->
+                    case atom of
+                        A ->
+                            [ A, B, A ]
+
+                        B ->
+                            [ B, B, B ]
+            }
+    in
+    ( { lsystem = lsystem, state = lsystem.axiom }
+    , Cmd.none
+    )
+
+
+
+-- SUBSCRIPTIONS
+
+
+keyDecoder : Decode.Decoder Msg
+keyDecoder =
+    Decode.map toKey (Decode.field "key" Decode.string)
+
+
+toKey : String -> Msg
+toKey string =
+    case String.uncons string of
+        Just ( 'r', "" ) ->
+            Reset
+
+        Just ( 'e', "" ) ->
+            Evolve
+
+        _ ->
+            Noop
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    onKeyPress keyDecoder
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Evolve
+    | Reset
+    | Noop
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Noop ->
+            ( model, Cmd.none )
+
+        Reset ->
+            ( { model | state = model.lsystem.axiom }, Cmd.none )
+
+        Evolve ->
+            ( { model | state = List.concatMap model.lsystem.rules model.state }, Cmd.none )
+
+
+
+-- VIEW
+
+
 atomView : Atom -> Svg.Svg msg
 atomView atom =
     circle
@@ -54,51 +159,6 @@ atomView atom =
         []
 
 
-
--- MODEL
-
-
-type alias Model =
-    List Atom
-
-
-
--- INIT
-
-
-init : () -> ( Model, Cmd Msg )
-init () =
-    ( [ A ], Cmd.none )
-
-
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
-
-
-
--- UPDATE
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        Noop ->
-            ( model, Cmd.none )
-
-
-type Msg
-    = Noop
-
-
-
--- VIEW
-
-
 view : Model -> Html Msg
 view model =
     svg
@@ -106,4 +166,4 @@ view model =
         , Svg.Attributes.height (String.fromInt height)
         , viewBox (String.join " " [ "0", "0", String.fromInt width, String.fromInt height ])
         ]
-        (List.map atomView model)
+        (List.map atomView model.state)
